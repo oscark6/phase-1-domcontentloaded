56184979bec12c0046dc3b7ac7f61d6b01dffa57
Oscar <oscarkimani86@icloud.com>
2024-04-22 10:24:00 +0300

    dom contentloaded

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Your code goes here
+
+document.addEventListener('DOMContentLoaded', function() {
+    let element = document.getElementById('text');
+    if (element) {
+      element.textContent = 'This is really cool!';
+    }
+  });
